28d2c71c698a5e6361776172dc5c45f40979bf10
Siedrix <Siedrix@gmail.com>
2011-04-16 12:04:50 -0500

    Added simplest loader

--- config.js
+++ config.js
@@ -1,15 +1,25 @@
 config = {
-	'status':'dev'
+	'status':'production'
 }
 
 production = {
 	scripts : {
-		'jquery':'http://code.jquery.com/jquery-latest.js'
+		'jquery':'http://code.jquery.com/jquery-latest.js',
+		'jquery.tmpl':'http://ajax.microsoft.com/ajax/jquery.templates/beta1/jquery.tmpl.min.js',
+		'underscore':'http://documentcloud.github.com/underscore/underscore-min.js'
+	}
+}
+
+development = {
+	scripts : {
+		'jquery':'src/jquery-1.5.2.js',
+		'jquery.tmpl':'src/jquery.tmpl.js',
+		'underscore':'src/underscore-min.js'
 	}
 }
 
 yepnope({
 	test: config.status == 'production',
-	yep: 'http://code.jquery.com/jquery-latest.js',
-	nope: 'src/jquery-1.5.2.js'
+	yep: production.scripts,
+	nope: development.scripts
 })
